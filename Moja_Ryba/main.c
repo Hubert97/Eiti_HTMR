86f792b39a739286b551639084526747ceb32665
Hubert97 <hubert.kowalski10@gmail.com>
2018-04-06 18:21:03 +0200

    ryba

---
+++ Moja_Ryba/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Tile
+{
+int Penguin_no
+int Fish_no;
+int if_available;
+};
+
+struct Penguin
+{
+int pos_X, pos_Y;
+int Player_No;
+};
+
+struct Player
+{
+char[256] Name;
+int Number_;
+int Cought_Fish_;
+};
+
+int main()
+{
+
+
+    printf("Hello world!\n");
+    return 0;
+}
