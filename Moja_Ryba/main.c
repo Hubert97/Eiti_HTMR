adefa88a346134f03f8650ebaf169913f61d1dde
deikare <bbgolawski@gmail.com>
2018-04-07 13:41:12 +0200

    ze wskaznikiem na gracza

--- Moja_Ryba/main.c
+++ Moja_Ryba/main.c
@@ -4,23 +4,23 @@
 
 typedef struct Tile
 {
-int Player_no;
-int Fish_no;
-int if_available;
+    int Player_no;
+    int Fish_no;
+    int if_available;
 } Pole;
-
-typedef struct Penguin
-{
-int pos_X, pos_Y;
-int Player_No;
-} Pengu;
 
 typedef struct Player
 {
-char Name[NAME_MAX];
-int Number_;
-int Cought_Fish_;
+    char Name[NAME_MAX];
+    int Number_;
+    int Cought_Fish_;
 } Gracz;
+
+typedef struct Penguin
+{
+    int pos_X, pos_Y;
+    Gracz* Owner;
+} Pengu;
 
 int main()
 {
