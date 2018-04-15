2dadc7f4834ff91b09ed15f9a13f007d8acaca5a
Hubert97 <hubert.kowalski10@gmail.com>
2018-04-15 14:55:06 +0200

    update

--- Moja_Ryba/main.c
+++ Moja_Ryba/main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Input_Output.h"
+#include "Board.h"
 #define NAME_MAX 256
 
-typedef struct
-{
-int Player_no;
-int Fish_no;
-int if_available;
-} Tile;
 
 typedef struct
 {
@@ -23,8 +19,14 @@ Penguin *Penguins;
 int amount_of_penguins;
 } Player;
 
+
 int main()
 {
+
+    Board *Board;
+    char Input_File_Name[]="Input.txt";
+    char Output_File_Name[] = "Output.txt";
+    Load( Board, Input_File_Name);
 
     Player c;
     c.Cought_Fish_=2;
